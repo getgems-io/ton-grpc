b59c38404b20d224ea07c74d35b71a0a7ec78a0b
Andrey Kostylev <kostylev.andrey.113@gmail.com>
2022-05-22 03:01:28 +0300

    blocks section

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,222 @@
-use crate::tonlib::{AccountTransactionId, AsyncClient, ClientBuilder, ShortTxId};
-use futures::future::join_all;
-use futures::{stream, Stream, StreamExt};
-use std::sync::Arc;
-use std::time::{Duration, Instant};
+use jsonrpc_core::{BoxFuture, Params};
+use crate::tonlib::{AsyncClient, base64_to_hex, BlockIdExt, ClientBuilder, ShortTxId, TlBlock};
+use jsonrpc_http_server::jsonrpc_core::IoHandler;
+use jsonrpc_http_server::tokio::runtime::Runtime;
+use jsonrpc_http_server::{ServerBuilder};
+use jsonrpc_derive::rpc;
+use jsonrpc_core::{Result, Error};
+use serde_json::{json, Value};
+use serde::Deserialize;
+use futures::StreamExt;
+
+#[macro_use]
+extern crate lazy_static;
 
 mod tonlib;
 
-#[tokio::main(flavor = "multi_thread")]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let client = Arc::new(
-        ClientBuilder::from_file("./liteserver_config.json")?
-            .disable_logging()
-            .build()
-            .await?,
-    );
+lazy_static! {
+    static ref TON: AsyncClient = {
+        let client = Runtime::new().unwrap().block_on(async {
+            ClientBuilder::from_file("./liteserver_config.json")
+                .unwrap()
+                .disable_logging()
+                .build()
+                .await
+                .unwrap()
+        });
 
-    let now = Instant::now();
-    let info = client.get_masterchain_info().await;
-    // let txs = get_tx_from_seqno(&client, info.last.seqno, info.last.seqno).await;
-    let tw = stream::iter((info.last.seqno - 1000)..info.last.seqno)
-        .then(|seqno| {
-            let client = &client;
-
-            async move { get_tx_from_seqno(client, seqno, info.last.seqno) }
-        })
-        .buffer_unordered(1000)
-        .map(|r| r.unwrap_or(vec![]))
-        .concat()
-        .await;
-
-    println!("{:?}", tw);
-
-    // stream::iter((info.last.seqno - 10000)..info.last.seqno)
-    //     .then(|seqno| async {
-    //         let client = client.clone();
-    //
-    //         get_tx_from_seqno(client, seqno)
-    //     }).collect::<Vec<ShortTxId>>();
-
-    // for seqno in info.last.seqno - 1000 .. info.last.seqno {
-    //     let shards = client.get_shards(seqno, 0).await;
-    //     for shard in shards {
-    //         let txs = client.get_transactions(&shard).await;
-    //         println!("{}, block contains {:#?} transactions", seqno, txs.len());
-    //     }
-    // }
-
-    println!("{}", (Instant::now() - now).as_secs_f64());
-
-    Ok(())
+        client
+    };
 }
 
-async fn get_tx_from_seqno(
-    client: &AsyncClient,
+#[derive(Deserialize, Debug)]
+struct LookupBlockParams {
+    workchain: i64,
+    shard: String,
+    seqno: Option<u64>,
+    lt: Option<i64>,
+    unixtime: Option<u64>
+}
+
+#[derive(Deserialize, Debug)]
+struct ShardsParams {
+    seqno: u64
+}
+
+#[derive(Deserialize, Debug)]
+struct BlockHeaderParams {
+    workchain: i64,
+    shard: String,
     seqno: u64,
-    max: u64,
-) -> anyhow::Result<Vec<ShortTxId>> {
-    let shards = client.get_shards(seqno, 0).await?;
-    let txs = stream::iter(shards).flat_map(|shard| client.get_tx_stream(shard));
-
-    let txs = txs.collect::<Vec<ShortTxId>>().await;
-
-    println!(
-        "{}/{}, block contains {:#?} transactions",
-        seqno,
-        max,
-        txs.len()
-    );
-
-    return Ok(txs);
+    root_hash: Option<String>,
+    file_hash: Option<String>
 }
+
+#[derive(Deserialize, Debug)]
+struct BlockTransactionsParams {
+    workchain: i64,
+    shard: String,
+    seqno: u64,
+    root_hash: Option<String>,
+    file_hash: Option<String>,
+    after_lt: Option<i64>,
+    after_hash: Option<String>,
+    count: Option<u8>
+}
+
+#[rpc(server)]
+pub trait Rpc {
+    #[rpc(name = "getMasterchainInfo")]
+    fn master_chain_info(&self) -> BoxFuture<Result<Value>>;
+
+    #[rpc(name = "lookupBlock", raw_params)]
+    fn lookup_block(&self, params: Params) -> BoxFuture<Result<Value>>;
+
+    #[rpc(name = "shards", raw_params)]
+    fn shards(&self, params: Params) -> BoxFuture<Result<Value>>;
+
+    #[rpc(name = "getBlockHeader", raw_params)]
+    fn get_block_header(&self, params: Params) -> BoxFuture<Result<Value>>;
+
+    #[rpc(name = "getBlockTransactions", raw_params)]
+    fn get_block_transactions(&self, params: Params) -> BoxFuture<Result<Value>>;
+}
+
+struct RpcImpl;
+
+impl Rpc for RpcImpl {
+    fn master_chain_info(&self) -> BoxFuture<Result<Value>> {
+        Box::pin(async {
+            jsonrpc_error(TON.get_masterchain_info().await)
+        })
+    }
+
+    fn lookup_block(&self, params: Params) -> BoxFuture<Result<Value>> {
+        Box::pin(async move {
+            let params = params.parse::<LookupBlockParams>()?;
+
+            let workchain = params.workchain;
+            let shard = params.shard.parse::<i64>().map_err(|_| Error::invalid_params("invalid shard"))?;
+            match (params.seqno, params.lt, params.unixtime) {
+                (Some(seqno), None, None) if seqno > 0 => jsonrpc_error(TON.look_up_block_by_seqno(workchain, shard, seqno).await),
+                (None, Some(lt), None) if lt > 0 => jsonrpc_error(TON.look_up_block_by_lt(workchain, shard, lt).await),
+                (None, None, Some(_)) => Err(Error::invalid_params("unixtime is not supported")),
+                _ => Err(Error::invalid_params("seqno or lt or unixtime must be provided"))
+            }
+        })
+    }
+
+    fn shards(&self, params: Params) -> BoxFuture<Result<Value>> {
+        Box::pin(async move {
+            let params = params.parse::<ShardsParams>()?;
+
+            jsonrpc_error(TON.get_shards(params.seqno).await)
+        })
+    }
+
+    fn get_block_header(&self, params: Params) -> BoxFuture<Result<Value>> {
+        Box::pin(async move {
+            let params = params.parse::<BlockHeaderParams>()?;
+            let shard = params.shard.parse::<i64>().map_err(|_| Error::invalid_params("invalid shard"))?;
+
+
+            jsonrpc_error(TON.get_block_header(
+                params.workchain,
+                shard,
+                params.seqno
+            ).await)
+        })
+    }
+
+    fn get_block_transactions(&self, params: Params) -> BoxFuture<jsonrpc_core::Result<Value>> {
+        Box::pin(async move {
+            let params = params.parse::<BlockTransactionsParams>()?;
+            let shard = params.shard.parse::<i64>().map_err(|_| Error::invalid_params("invalid shard"))?;
+            let count = params.count.unwrap_or(200);
+
+            let block_json = TON
+                .look_up_block_by_seqno(params.workchain, shard, params.seqno)
+                .await.map_err(|_| Error::internal_error())?;
+
+            let block = serde_json::from_value::<BlockIdExt>(block_json)
+                .map_err(|_| Error::internal_error())?;
+
+            let stream = TON.get_tx_stream(block.clone()).await.map_err(|_| Error::internal_error())?;
+            let tx: Vec<TlBlock> = stream
+                .map(|tx: ShortTxId| TlBlock::ShortTxId(ShortTxId {
+                    account: format!("{}:{}", block.workchain, base64_to_hex(&tx.account).unwrap()),
+                    hash: tx.hash,
+                    lt: tx.lt,
+                    mode: tx.mode
+                }))
+                .collect()
+                .await;
+
+
+            Ok(json!({
+                "@type": "blocks.transactions",
+                "id": TlBlock::BlockIdExt(block),
+                "incomplete": false,
+                "req_count": count,
+                "transactions": &tx
+            }))
+        })
+    }
+}
+
+fn jsonrpc_error<T>(r: anyhow::Result<T>) -> jsonrpc_core::Result<T> {
+    r.map_err(|_| jsonrpc_core::Error::internal_error())
+}
+
+fn main() {
+    let mut rt = Runtime::new().unwrap();
+    println!("{:?}", rt.block_on(TON.get_masterchain_info()));
+
+    let mut io = IoHandler::new();
+    io.extend_with(RpcImpl.to_delegate());
+
+    let server = ServerBuilder::new(io)
+        .event_loop_executor(rt.handle().clone())
+        .threads(1)
+        .start_http(&"127.0.0.1:3030".parse().unwrap())
+        .unwrap();
+
+    server.wait()
+}
+
+// #[tokio::main(flavor = "multi_thread")]
+// async fn main() -> Result<(), Box<dyn std::error::Error>> {
+//     let client = Arc::new(
+//         ClientBuilder::from_file("./liteserver_config.json")?
+//             .disable_logging()
+//             .build()
+//             .await?,
+//     );
+//
+//     let _ = tokio::task::spawn_blocking({
+//         let client = client.clone();
+//
+//         move || {
+//             let mut io = jsonrpc_core::IoHandler::new();
+//             let client = client.clone();
+//             io.add_method("getMasterchainInfo", {
+//                 move |_params: Params| async {
+//                     client
+//                         .get_masterchain_info()
+//                         .await
+//                         .map_err(|e| jsonrpc_core::Error::new(ServerError(1)))
+//                 }
+//             });
+//
+//             let server = ServerBuilder::new(io)
+//                 .threads(3)
+//                 .start_http(&"127.0.0.1:3030".parse().unwrap())
+//                 .unwrap();
+//
+//             server.wait();
+//         }
+//     })
+//     .await;
+//
+//     Ok(())
+// }
--- src/tonlib/mod.rs
+++ src/tonlib/mod.rs
@@ -1,27 +1,27 @@
+use async_stream::stream;
+use futures::StreamExt;
+use futures::{pin_mut, stream, FutureExt, Stream};
+use serde::de::DeserializeOwned;
+use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
 use std::borrow::Borrow;
 use std::collections::HashMap;
 use std::error::Error;
-use std::fmt::{Display, format, Formatter, write};
+use std::fmt::{format, write, Display, Formatter};
 use std::fs::File;
 use std::io::BufReader;
 use std::path::{Path, PathBuf};
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
-use async_stream::stream;
-use futures::{FutureExt, pin_mut, Stream, stream};
-use serde_json::{json, Value};
-use serde::{Serialize, Deserialize};
-use serde::de::DeserializeOwned;
-use futures::StreamExt;
 use tokio::sync::oneshot::error::RecvError;
 use tokio::sync::Semaphore;
-use uuid::Uuid;
 use tonlibjson_rs::Client;
+use uuid::Uuid;
 
 pub struct ClientBuilder {
-    config: serde_json::Value,
-    disable_logging: Option<serde_json::Value>,
+    config: Value,
+    disable_logging: Option<Value>,
 }
 
 impl ClientBuilder {
@@ -79,8 +79,7 @@ impl ClientBuilder {
 }
 
 const MAIN_WORKCHAIN: i64 = -1;
-const MAIN_SHARD: &str = "-9223372036854775808";
-
+const MAIN_SHARD: i64 = -9223372036854775808;
 
 pub trait TlType {
     fn tl_type() -> String;
@@ -88,10 +87,10 @@ pub trait TlType {
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct ShortTxId {
-    account: String,
-    hash: String,
-    lt: String,
-    mode: u8,
+    pub account: String,
+    pub hash: String,
+    pub lt: String,
+    pub mode: u8,
 }
 
 #[derive(Debug, Deserialize)]
@@ -102,7 +101,11 @@ pub struct TonError {
 
 impl Display for TonError {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Ton api error occurred with code {}, message {}", self.code, self.message)
+        write!(
+            f,
+            "Ton api error occurred with code {}, message {}",
+            self.code, self.message
+        )
     }
 }
 
@@ -111,7 +114,6 @@ impl Error for TonError {
         None
     }
 }
-
 
 impl ShortTxId {
     fn ton_type() -> String {
@@ -149,9 +151,11 @@ impl TlType for MasterchainInfoResponse {
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(tag = "@type")]
-enum TlBlock {
+pub enum TlBlock {
     #[serde(rename = "ton.blockIdExt")]
     BlockIdExt(BlockIdExt),
+    #[serde(rename = "blocks.shortTxId")]
+    ShortTxId(ShortTxId),
     #[serde(rename = "blocks.getMasterchainInfo")]
     GetMasterchainInfoRequest,
     // #[serde(rename = "blocks.masterchainInfo")]
@@ -206,7 +210,8 @@ impl AsyncClient {
         let client_recv = client.clone();
         let semaphore = tokio::sync::Semaphore::new(500);
 
-        let responses: Arc<Mutex<HashMap<String, tokio::sync::oneshot::Sender<Value>>>> = Arc::new(Mutex::new(HashMap::new()));
+        let responses: Arc<Mutex<HashMap<String, tokio::sync::oneshot::Sender<Value>>>> =
+            Arc::new(Mutex::new(HashMap::new()));
         let responses_rcv = Arc::clone(&responses);
 
         let _ = thread::spawn(move || {
@@ -238,7 +243,7 @@ impl AsyncClient {
         let _ = self.client.send(&request.to_string());
     }
 
-    async fn execute(&self, request: &serde_json::Value) -> Result<Value, RecvError> {
+    async fn execute(&self, request: &Value) -> anyhow::Result<Value> {
         let mut request = request.clone();
 
         let id = Uuid::new_v4().to_string();
@@ -251,13 +256,21 @@ impl AsyncClient {
         println!("{} available permits", self.semaphore.available_permits());
         let _ = self.client.send(&x);
 
-        let response = rx.await;
+        let mut value = rx.await?;
         drop(permit);
 
-        return response;
+        let mut obj = value.as_object_mut().unwrap();
+        let _ = obj.remove("@extra");
+
+        let value = Value::Object(obj.clone());
+
+        return Ok(value);
     }
 
-    async fn execute_typed<T: DeserializeOwned>(&self, request: &serde_json::Value) -> anyhow::Result<T> {
+    async fn execute_typed<T: DeserializeOwned>(
+        &self,
+        request: &serde_json::Value,
+    ) -> anyhow::Result<T> {
         let mut request = request.clone();
 
         let id = Uuid::new_v4().to_string();
@@ -279,66 +292,109 @@ impl AsyncClient {
         if value["@type"] == "error" {
             return match serde_json::from_value::<TonError>(value) {
                 Ok(e) => Err(anyhow::Error::from(e)),
-                Err(e) => Err(anyhow::Error::from(e))
+                Err(e) => Err(anyhow::Error::from(e)),
             };
         }
 
         return serde_json::from_value::<T>(value).map_err(anyhow::Error::from);
     }
 
-    pub async fn get_masterchain_info(&self) -> MasterchainInfoResponse {
+    pub async fn get_masterchain_info(&self) -> anyhow::Result<Value> {
         let query = json!(TlBlock::GetMasterchainInfoRequest);
 
-        return self.execute_typed::<MasterchainInfoResponse>(&query).await.unwrap();
+        return self.execute(&query).await;
     }
 
-    pub async fn look_up_block_by_seqno(&self, workchain: i64, shard: &str, seqno: u64) -> BlockIdExt {
+    pub async fn look_up_block_by_seqno(
+        &self,
+        workchain: i64,
+        shard: i64,
+        seqno: u64,
+    ) -> anyhow::Result<Value> {
         return self.look_up_block(workchain, shard, seqno, 0).await;
     }
 
-    pub async fn look_up_block_by_lt(&self, workchain: i64, shard: &str, lt: i64) -> BlockIdExt {
+    pub async fn look_up_block_by_lt(&self, workchain: i64, shard: i64, lt: i64) -> anyhow::Result<Value> {
         return self.look_up_block(workchain, shard, 0, lt).await;
     }
 
-    pub async fn get_shards(&self, master_seqno: u64, lt: i64) -> anyhow::Result<Vec<BlockIdExt>> {
-        let block = self.look_up_block(MAIN_WORKCHAIN, MAIN_SHARD, master_seqno, lt).await;
+    pub async fn get_shards(&self, master_seqno: u64) -> anyhow::Result<Value> {
+        let block = self
+            .look_up_block(MAIN_WORKCHAIN, MAIN_SHARD, master_seqno, 0)
+            .await?;
         let request = json!({
             "@type": "blocks.getShards",
             "id": block
         });
 
-        return Ok(self.execute_typed::<ShardsResponse>(&request).await?.shards);
+        return self.execute(&request).await;
     }
 
-    pub fn get_tx_stream(&self, block: BlockIdExt) -> impl Stream<Item=ShortTxId> + '_ {
+    pub async fn get_block_header(&self, workchain: i64, shard: i64, seqno: u64) -> anyhow::Result<Value> {
+        let block = self
+            .look_up_block(workchain, shard, seqno, 0)
+            .await?;
+
+        let request = json!({
+            "@type": "blocks.getBlockHeader",
+            "id": block
+        });
+
+        return self.execute(&request).await;
+    }
+
+    pub async fn get_tx_stream(&self, block: BlockIdExt) -> anyhow::Result<impl Stream<Item = ShortTxId> + '_> {
         struct State {
             last_tx: Option<AccountTransactionId>,
             incomplete: bool,
-            block: BlockIdExt
+            block: BlockIdExt,
         }
 
-        return stream::unfold(State {last_tx: None, incomplete: true, block }, {
-            move |state: State| async move {
-            if state.incomplete == false {
-                return None;
-            }
+        return Ok(stream::unfold(
+            State {
+                last_tx: None,
+                incomplete: true,
+                block,
+            },
+            {
+                move |state: State| async move {
+                    if state.incomplete == false {
+                        return None;
+                    }
 
-            let txs;
-            if let Some(tx) = state.last_tx {
-                txs = self._get_transactions_after(&state.block, 30, tx).await.unwrap();
-            } else {
-                txs = self._get_transactions(&state.block, 30).await.unwrap();
-            }
+                    let txs;
+                    if let Some(tx) = state.last_tx {
+                        txs = self
+                            ._get_transactions_after(&state.block, 30, tx)
+                            .await
+                            .unwrap();
+                    } else {
+                        txs = self._get_transactions(&state.block, 30).await.unwrap();
+                    }
 
-            println!("got {} transactions", txs.transactions.len());
+                    println!("got {} transactions", txs.transactions.len());
 
-            let last_tx = txs.transactions.last().map(AccountTransactionId::from);
+                    let last_tx = txs.transactions.last().map(AccountTransactionId::from);
 
-            return Some((stream::iter(txs.transactions), State {last_tx, incomplete: txs.incomplete, block: state.block}));
-        }}).flatten();
+                    return Some((
+                        stream::iter(txs.transactions),
+                        State {
+                            last_tx,
+                            incomplete: txs.incomplete,
+                            block: state.block,
+                        },
+                    ));
+                }
+            },
+        )
+        .flatten());
     }
 
-    async fn _get_transactions(&self, block: &BlockIdExt, count: u32) -> anyhow::Result<TransactionsResponse> {
+    async fn _get_transactions(
+        &self,
+        block: &BlockIdExt,
+        count: u32,
+    ) -> anyhow::Result<TransactionsResponse> {
         let request = json!({
             "@type": "blocks.getTransactions",
             "id": block,
@@ -353,7 +409,12 @@ impl AsyncClient {
         return self.execute_typed(&request).await;
     }
 
-    async fn _get_transactions_after(&self, block: &BlockIdExt, count: u32, tx: AccountTransactionId) -> anyhow::Result<TransactionsResponse> {
+    async fn _get_transactions_after(
+        &self,
+        block: &BlockIdExt,
+        count: u32,
+        tx: AccountTransactionId,
+    ) -> anyhow::Result<TransactionsResponse> {
         let block = TlBlock::BlockIdExt(block.clone());
         let tx = TlBlock::AccountTransactionId(tx);
 
@@ -368,10 +429,14 @@ impl AsyncClient {
         return self.execute_typed(&request).await;
     }
 
-    async fn look_up_block(&self, workchain: i64, shard: &str, seqno: u64, lt: i64) -> BlockIdExt {
+    async fn look_up_block(&self, workchain: i64, shard: i64, seqno: u64, lt: i64) -> anyhow::Result<Value> {
         let mut mode: i32 = 0;
-        if seqno > 0 { mode += 1 }
-        if lt > 0 { mode += 2 }
+        if seqno > 0 {
+            mode += 1
+        }
+        if lt > 0 {
+            mode += 2
+        }
 
         let query = json!({
             "@type": "blocks.lookupBlock",
@@ -385,6 +450,14 @@ impl AsyncClient {
             "lt": lt
         });
 
-        return self.execute_typed(&query).await.unwrap();
+        return self.execute(&query).await;
     }
+}
+
+
+pub fn base64_to_hex(b: &str) -> anyhow::Result<String> {
+    let bytes = base64::decode(b)?;
+    let hex = hex::encode(bytes);
+
+    return Ok(hex)
 }
