7b1812c330d45272c91a109023ab609b17d0a5db
Andrey Kostylev <a.kostylev@protonmail.com>
2022-06-09 23:48:54 +0300

    optimize build (#13)
    * slim docker image
    
    * fix clippy warn
    
    * optimize build
    
    * up github/codeql-action/upload-sarif

--- tonlibjson-rs/build.rs
+++ tonlibjson-rs/build.rs
@@ -1,57 +1,57 @@
 use std::env;
+use cmake::Config;
 
 fn main() {
-    let is_static = env::var("PROFILE").unwrap_or_default() == "release";
-    let is_lto = is_static; // TODO
+    let is_release = env::var("PROFILE").unwrap() == "release";
+    let target = env::var("TARGET").unwrap();
+    let is_darwin = target == "x86_64-apple-darwin";
 
-    if !is_static {
-        let dst = cmake::Config::new("ton")
-            .define("TON_ONLY_TONLIB", "ON")
-            .build_target("tonlibjson")
-            .build();
+    let openssl_dir = env::var("OPENSSL_ROOT_DIR")
+        .ok()
+        .map(|x| format!("{}/lib", x))
+        .or_else(
+            || pkg_config::probe_library("openssl")
+                .ok()
+                .map(|lib| lib.link_paths.first().unwrap().display().to_string()
+                )
+        ).unwrap();
 
-        println!("cargo:rustc-link-search=native={}/build/tonlib", dst.display());
-        println!("cargo:rustc-link-lib=dylib=tonlibjson");
-
-        return;
-    }
-
-    let openssl_dir = pkg_config::probe_library("openssl").ok()
-            .map(|lib| lib.link_paths.first().unwrap().display().to_string()).or_else(
-        || env::var("OPENSSL_ROOT_DIR").ok().map(|x| format!("{}/lib", x))
-    ).unwrap();
-
-    println!("cargo:rustc-link-arg=-fuse-ld=lld");
-    println!("cargo:rustc-link-search=native={}", openssl_dir);
-    println!("cargo:rustc-link-lib=static=crypto");
-    println!("cargo:rustc-link-lib=static=ssl");
-
-    let dst= if is_lto {
-        cmake::Config::new("ton")
-            .uses_cxx11()
-            .cxxflag("-flto")
+    let dst= if !is_darwin && is_release {
+        Config::new("ton")
             .define("TON_ONLY_TONLIB", "ON")
             .define("CMAKE_C_COMPILER", "clang")
             .define("CMAKE_CXX_COMPILER", "clang++")
+            .define("CMAKE_CXX_STANDARD", "14")
             .cxxflag("-std=c++14")
             .cxxflag("-stdlib=libc++")
             .cxxflag("-fuse-ld=lld")
             .cxxflag("-Wno-error=unused-command-line-argument")
+            .cxxflag("-flto")
+            .uses_cxx11()
             .build_target("tonlibjson_static")
             .build()
     } else {
-        cmake::Config::new("ton")
+        Config::new("ton")
             .uses_cxx11()
             .define("TON_ONLY_TONLIB", "ON")
             .define("CMAKE_C_COMPILER", "clang")
             .define("CMAKE_CXX_COMPILER", "clang++")
+            .define("CMAKE_CXX_STANDARD", "14")
             .cxxflag("-std=c++14")
             .cxxflag("-stdlib=libc++")
             .build_target("tonlibjson_static")
             .build()
     };
 
-    println!("cargo:rustc-link-lib=static=c++");
+    if is_darwin {
+        println!("cargo:rustc-link-lib=dylib=c++");
+    } else {
+        println!("cargo:rustc-link-lib=static=c++");
+    }
+
+    println!("cargo:rustc-link-search=native={}", openssl_dir);
+    println!("cargo:rustc-link-lib=static=crypto");
+    println!("cargo:rustc-link-lib=static=ssl");
 
     for item in ["tdnet", "keys", "tdactor", "tl-utils", "tdutils"] {
         println!("cargo:rustc-link-search=native={}/build/{}", dst.display(), item);
