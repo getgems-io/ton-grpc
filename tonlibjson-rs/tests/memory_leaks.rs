6884c5f0a27e118e137526b368da8298f41fbbd2
Andrei Kostylev <a.kostylev@protonmail.com>
2022-11-12 17:39:59 +0300

    add memleak test for tonlibjson

---
+++ tonlibjson-rs/tests/memory_leaks.rs
@@ -0,0 +1,30 @@
+use tonlibjson_rs::Client;
+
+#[test]
+fn test_mem() {
+    let memory_before = procinfo::pid::statm_self().unwrap().resident;
+
+    {
+        let mut clients = vec![];
+        for _ in 1..10000 {
+            let client = Client::new();
+            clients.push(client);
+
+            if clients.len() > 32 {
+                clients.pop();
+            }
+        }
+
+        drop(clients);
+    }
+
+    let memory_after = procinfo::pid::statm_self().unwrap().resident;
+
+    assert!(
+        memory_after < memory_before + 4096,
+        "Memory usage at server start is {}KB, memory usage after is {}KB. Diff is {}KB",
+        memory_before,
+        memory_after,
+        memory_after - memory_before
+    );
+}
