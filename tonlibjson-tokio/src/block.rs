133902dd7b398c6e1426feff3dc5d1cd0f821988
Andrei Kostylev <a.kostylev@protonmail.com>
2022-11-24 10:52:39 +0300

    move tvm blocks to main crate

--- tonlibjson-tokio/src/block.rs
+++ tonlibjson-tokio/src/block.rs
@@ -124,3 +124,137 @@ impl From<&ShortTxId> for AccountTransactionId {
         }
     }
 }
+
+
+#[derive(Debug, Serialize)]
+#[serde(tag = "@type", rename = "smc.load")]
+pub struct SmcLoad {
+    pub account_address: AccountAddress
+}
+
+#[derive(Debug, Serialize)]
+#[serde(tag = "@type", rename = "smc.runGetMethod")]
+pub struct SmcRunGetMethod {
+    id: i64,
+    method: SmcMethodId,
+    stack: Vec<StackEntry>
+}
+
+#[derive(Debug, Serialize)]
+#[serde(tag = "@type")]
+pub enum SmcMethodId {
+    #[serde(rename = "smc.methodIdNumber")]
+    Number { number: i32 },
+    #[serde(rename = "smc.methodIdName")]
+    Name { name: String }
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(tag = "@type")]
+#[serde(rename = "tvm.slice")]
+pub struct Slice {
+    pub bytes: String
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(tag = "@type")]
+#[serde(rename = "tvm.cell")]
+pub struct Cell {
+    pub bytes: String
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(tag = "@type")]
+#[serde(rename = "tvm.numberDecimal")]
+pub struct Number {
+    pub number: String
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(tag = "@type")]
+#[serde(rename = "tvm.tuple")]
+pub struct Tuple {
+    pub elements: Vec<StackEntry>
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(tag = "@type")]
+#[serde(rename = "tvm.list")]
+pub struct List {
+    pub elements: Vec<StackEntry>
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(tag = "@type")]
+pub enum StackEntry {
+    #[serde(rename = "tvm.stackEntrySlice")]
+    Slice { slice: Slice },
+    #[serde(rename = "tvm.stackEntryCell")]
+    Cell { cell: Cell },
+    #[serde(rename = "tvm.stackEntryNumber")]
+    Number { number: Number },
+    #[serde(rename = "tvm.stackEntryTuple")]
+    Tuple { tuple: Tuple },
+    #[serde(rename = "tvm.stackEntryList")]
+    List { list: List },
+
+    #[serde(rename = "tvm.stackEntryUnsupported")]
+    Unsupported
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::block::{Cell, List, Number, Slice, StackEntry, Tuple, SmcMethodId};
+    use serde_json::json;
+
+    #[test]
+    fn slice_correct_json() {
+        let slice = Slice { bytes: "test".to_string() };
+
+        assert_eq!(serde_json::to_string(&slice).unwrap(), "{\"@type\":\"tvm.slice\",\"bytes\":\"test\"}")
+    }
+
+    #[test]
+    fn cell_correct_json() {
+        let cell = Cell { bytes: "test".to_string() };
+
+        assert_eq!(serde_json::to_string(&cell).unwrap(), "{\"@type\":\"tvm.cell\",\"bytes\":\"test\"}")
+    }
+
+    #[test]
+    fn number_correct_json() {
+        let number = Number { number: "100.2".to_string() };
+
+        assert_eq!(serde_json::to_string(&number).unwrap(), "{\"@type\":\"tvm.numberDecimal\",\"number\":\"100.2\"}")
+    }
+
+    #[test]
+    fn stack_entry_correct_json() {
+        let slice = StackEntry::Slice { slice: Slice { bytes: "test".to_string() }};
+        let cell = StackEntry::Cell { cell: Cell { bytes: "test".to_string() }};
+        let number = StackEntry::Number { number: Number { number: "123".to_string() }};
+        let tuple = StackEntry::Tuple { tuple: Tuple { elements: vec![slice.clone(), cell.clone()]  }};
+        let list = StackEntry::List { list: List { elements: vec![slice.clone(), tuple.clone()]  }};
+
+        assert_eq!(serde_json::to_string(&slice).unwrap(), "{\"@type\":\"tvm.stackEntrySlice\",\"slice\":{\"@type\":\"tvm.slice\",\"bytes\":\"test\"}}");
+        assert_eq!(serde_json::to_string(&cell).unwrap(), "{\"@type\":\"tvm.stackEntryCell\",\"cell\":{\"@type\":\"tvm.cell\",\"bytes\":\"test\"}}");
+        assert_eq!(serde_json::to_string(&number).unwrap(), "{\"@type\":\"tvm.stackEntryNumber\",\"number\":{\"@type\":\"tvm.numberDecimal\",\"number\":\"123\"}}");
+        assert_eq!(serde_json::to_string(&tuple).unwrap(), "{\"@type\":\"tvm.stackEntryTuple\",\"tuple\":{\"@type\":\"tvm.tuple\",\"elements\":[{\"@type\":\"tvm.stackEntrySlice\",\"slice\":{\"@type\":\"tvm.slice\",\"bytes\":\"test\"}},{\"@type\":\"tvm.stackEntryCell\",\"cell\":{\"@type\":\"tvm.cell\",\"bytes\":\"test\"}}]}}");
+        assert_eq!(serde_json::to_string(&list).unwrap(), "{\"@type\":\"tvm.stackEntryList\",\"list\":{\"@type\":\"tvm.list\",\"elements\":[{\"@type\":\"tvm.stackEntrySlice\",\"slice\":{\"@type\":\"tvm.slice\",\"bytes\":\"test\"}},{\"@type\":\"tvm.stackEntryTuple\",\"tuple\":{\"@type\":\"tvm.tuple\",\"elements\":[{\"@type\":\"tvm.stackEntrySlice\",\"slice\":{\"@type\":\"tvm.slice\",\"bytes\":\"test\"}},{\"@type\":\"tvm.stackEntryCell\",\"cell\":{\"@type\":\"tvm.cell\",\"bytes\":\"test\"}}]}}]}}");
+    }
+
+    #[test]
+    fn smc_method_id() {
+        let number = SmcMethodId::Number { number: 123 };
+        let name = SmcMethodId::Name { name: "getOwner".to_owned() };
+
+        assert_eq!(serde_json::to_value(&number).unwrap(), json!({
+            "@type": "smc.methodIdNumber",
+            "number": 123
+        }));
+        assert_eq!(serde_json::to_value(&name).unwrap(), json!({
+            "@type": "smc.methodIdName",
+            "name": "getOwner"
+        }));
+    }
+}
