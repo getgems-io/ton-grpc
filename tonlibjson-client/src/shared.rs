cd0c8a585a637b09143e1b108279c47369c39d99
Andrei Kostylev <a.kostylev@pm.me>
2023-05-12 22:00:19 +0300

    shared response future

--- tonlibjson-client/src/shared.rs
+++ tonlibjson-client/src/shared.rs
@@ -1,10 +1,12 @@
 use std::future::Future;
 use std::pin::Pin;
-use std::sync::Arc;
+use std::sync::{Arc, RwLock};
 use std::task::{Context, Poll};
+use futures::ready;
+use pin_project_lite::pin_project;
 use tower::{Layer, Service};
 use tower::load::Load;
-use tokio::sync::{RwLock};
+use crate::shared::ResponseState::Locking;
 
 #[derive(Default)]
 pub struct SharedLayer;
@@ -36,10 +38,14 @@ impl<S> SharedService<S> {
 }
 
 impl<S, Req> Service<Req> for SharedService<S>
-    where S : Service<Req> + Send + Sync + 'static, S::Future : Send, Req: Send + 'static {
+    where S : Service<Req> + Send + Sync + 'static,
+          S::Future : Send,
+          S::Error: Send + Sync + 'static,
+          S::Response: Send,
+          Req: Send + 'static {
     type Response = S::Response;
     type Error = S::Error;
-    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
+    type Future = ResponseFuture<S, Req>;
 
     fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
         match self.inner.try_write() {
@@ -54,19 +60,8 @@ impl<S, Req> Service<Req> for SharedService<S>
         }
     }
 
-    //TODO[akostylev0] ResponseFuture
     fn call(&mut self, req: Req) -> Self::Future {
-        use futures::FutureExt;
-
-        let client = Arc::clone(&self.inner);
-
-        async move {
-            let mut guard = client.write().await;
-            let r = guard.call(req);
-            drop(guard);
-
-            r.await
-        }.boxed()
+        ResponseFuture::new(&self.inner, req)
     }
 }
 
@@ -74,6 +69,82 @@ impl<S> Load for SharedService<S> where S : Load {
     type Metric = S::Metric;
 
     fn load(&self) -> Self::Metric {
-        tokio::task::block_in_place(|| self.inner.blocking_read().load())
+        self.inner.read().unwrap().load()
+    }
+}
+
+pin_project! {
+    pub struct ResponseFuture<S: Service<Req>, Req>
+    {
+        request: Option<Req>,
+        #[pin]
+        state: ResponseState<S, Req>
+    }
+}
+
+pin_project! {
+    #[project = ResponseStateProj]
+    enum ResponseState<S, Req> where S : Service<Req> {
+        Locking { service: Arc<RwLock<S>> },
+        Waiting {
+            #[pin]
+            future: S::Future
+        }
+    }
+}
+
+impl<S: Service<Req>, Req> ResponseFuture<S, Req> where
+    S: Service<Req> + Send + Sync + 'static,
+    S::Error: Send + Sync + 'static,
+    S::Response: Send + 'static,
+    S::Future: Send + 'static,
+    Req: Send + 'static
+{
+    pub fn new(service: &Arc<RwLock<S>>, request: Req) -> Self {
+        Self {
+            request: Some(request),
+            state: Locking { service: Arc::clone(service) }
+        }
+    }
+}
+
+impl<S: Service<Req>, Req> Future for ResponseFuture<S, Req> where
+    S: Service<Req> + Send + Sync + 'static,
+    S::Error: Send + Sync + 'static,
+    S::Response: Send + 'static,
+    S::Future: Send + 'static,
+    Req: Send + 'static
+{
+    type Output = Result<S::Response, S::Error>;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let mut this = self.project();
+
+        loop {
+            match this.state.as_mut().project() {
+                ResponseStateProj::Locking { service } => {
+                    let future;
+
+                    {
+                        if let Ok(mut guard) = service.try_write() {
+                            let req = this.request.take()
+                                .expect("Future was polled after completion");
+                            future = guard.call(req);
+                        } else {
+                            cx.waker().wake_by_ref();
+
+                            return Poll::Pending;
+                        }
+
+                        this.state.set(ResponseState::Waiting { future });
+                    }
+                }
+                ResponseStateProj::Waiting { future } => {
+                    let response = ready!(future.poll(cx));
+
+                    return Poll::Ready(response);
+                }
+            }
+        }
     }
 }
