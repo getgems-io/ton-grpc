6dadf8123397796d150183cee94b3363667a174d
Andrei Kostylev <a.kostylev@protonmail.com>
2023-11-08 15:07:37 +0300

    feat: ton-grpc ton config url

--- tonlibjson-client/src/ton.rs
+++ tonlibjson-client/src/ton.rs
@@ -31,12 +31,12 @@ use crate::session::RunGetMethod;
 use crate::shared::SharedService;
 
 #[cfg(not(feature = "testnet"))]
-pub(crate) fn default_ton_config_url() -> Url {
+pub fn default_ton_config_url() -> Url {
     Url::from_str("https://raw.githubusercontent.com/ton-blockchain/ton-blockchain.github.io/main/global.config.json").unwrap()
 }
 
 #[cfg(feature = "testnet")]
-pub(crate) fn default_ton_config_url() -> Url {
+pub fn default_ton_config_url() -> Url {
     Url::from_str("https://raw.githubusercontent.com/ton-blockchain/ton-blockchain.github.io/main/testnet-global.config.json").unwrap()
 }
 
