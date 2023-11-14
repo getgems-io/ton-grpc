2ff876a7078d2e7e77d8fb7d34a236c5f5132571
Andrei Kostylev <a.kostylev@pm.me>
2023-11-14 20:46:09 +0300

    refac

--- tonlibjson-client/src/router.rs
+++ tonlibjson-client/src/router.rs
@@ -28,18 +28,43 @@ impl Router {
         }
     }
 
-    fn update_pending_from_discover(
-        &mut self,
-        cx: &mut Context<'_>,
-    ) -> Poll<Option<Result<(), anyhow::Error>>> {
+    fn update_pending_from_discover(&mut self, cx: &mut Context<'_>, ) -> Poll<Option<Result<(), anyhow::Error>>> {
         loop {
-            match ready!(Pin::new(&mut self.discover).poll_discover(cx))
-                .transpose()
-                .map_err(|e| anyhow!(e))?
-            {
+            match ready!(Pin::new(&mut self.discover).poll_discover(cx)).transpose()? {
                 None => return Poll::Ready(None),
                 Some(Change::Remove(key)) => { self.services.remove(&key); }
                 Some(Change::Insert(key, svc)) => { self.services.insert(key, svc); }
+            }
+        }
+    }
+
+    fn choose(&self, route: &Route) -> Vec<CursorClient> {
+        match route {
+            Route::Block { chain, criteria} => {
+                self.services
+                    .values()
+                    .filter(|s| s.contains(chain, criteria).is_some_and(|b| b <= 1))
+                    .cloned()
+                    .collect()
+            },
+            Route::Latest => {
+                let groups = self.services
+                    .values()
+                    .filter_map(|s| s.last_seqno().map(|seqno| (s, seqno)))
+                    .sorted_unstable_by_key(|(_, seqno)| -seqno)
+                    .group_by(|(_, seqno)| *seqno);
+
+                let mut idxs = vec![];
+                for (_, group) in &groups {
+                    idxs = group.collect();
+
+                    // we need at least 3 nodes in group
+                    if idxs.len() > 2 {
+                        break;
+                    }
+                }
+
+                idxs.into_iter().map(|(s, _)| s).cloned().collect()
             }
         }
     }
@@ -75,7 +100,7 @@ impl Service<&Route> for Router {
     }
 
     fn call(&mut self, req: &Route) -> Self::Future {
-        let services = req.choose(self.services.values());
+        let services = self.choose(&req);
 
         let response = if services.is_empty() {
             metrics::increment_counter!("ton_router_miss_count");
@@ -86,36 +111,5 @@ impl Service<&Route> for Router {
         };
 
         std::future::ready(response)
-    }
-}
-
-impl Route {
-    pub(crate) fn choose<'a, T : Iterator<Item=&'a CursorClient>>(&self, services: T) -> Vec<CursorClient> {
-        match self {
-            Route::Block { chain, criteria} => {
-                services
-                    .filter(|s| s.contains(chain, criteria).is_some_and(|b| b <= 1))
-                    .cloned()
-                    .collect()
-            },
-            Route::Latest => {
-                let groups = services
-                    .filter_map(|s| s.last_seqno().map(|seqno| (s, seqno)))
-                    .sorted_unstable_by_key(|(_, seqno)| -seqno)
-                    .group_by(|(_, seqno)| *seqno);
-
-                let mut idxs = vec![];
-                for (_, group) in &groups {
-                    idxs = group.collect();
-
-                    // we need at least 3 nodes in group
-                    if idxs.len() > 2 {
-                        break;
-                    }
-                }
-
-                idxs.into_iter().map(|(s, _)| s).cloned().collect()
-            }
-        }
     }
 }
