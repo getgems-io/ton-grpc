c98fa75bf129525cc12a9f182befecd0682a1330
Andrei Kostylev <a.kostylev@pm.me>
2023-11-16 01:25:35 +0300

    args parse

--- tl_parser/src/lib.rs
+++ tl_parser/src/lib.rs
@@ -1,12 +1,13 @@
 use anyhow::anyhow;
 use nom::branch::alt;
 use nom::bytes::complete::{is_not, tag, take_till, take_until, take_while, take_while1, take_while_m_n};
-use nom::character::complete::{char, line_ending, multispace0, multispace1, newline, satisfy};
+use nom::character::complete::{char, line_ending, multispace0, multispace1, newline, satisfy, space0};
 use nom::combinator::{map, opt, recognize, value};
 use nom::error::Error;
-use nom::multi::{fold_many0, many0};
+use nom::multi::{fold_many0, many0, many1};
 use nom::Parser;
 use nom::sequence::{delimited, pair, preceded, terminated};
+use crate::FieldType::{Bare, ConditionalField, Repetition};
 
 pub type ConstructorNumber = u32;
 
@@ -16,6 +17,27 @@ pub struct Combinator {
     id: String,
     r#type: String,
     constructor_number: Option<ConstructorNumber>,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum FieldType {
+    ConditionalField {
+        condition: Option<String>,
+        r#type: String
+    },
+    Repetition {
+        multiplicity: Option<String>,
+        fields: Vec<Field>,
+    },
+    Bare {
+        name: String
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Field {
+    name: Option<String>,
+    r#type: FieldType,
 }
 
 pub fn parse(input: &str) -> anyhow::Result<Vec<Combinator>> {
@@ -63,14 +85,14 @@ fn space_or_comment(input: &str) -> nom::IResult<&str, ()> {
 
 fn lc_ident(input: &str) -> nom::IResult<&str, String> {
     let (input, head) = satisfy(is_lc_letter)(input)?;
-    let (input, tail) = take_while1(is_ident_char)(input)?;
+    let (input, tail) = take_while(is_ident_char)(input)?;
 
     Ok((input, format!("{}{}", head, tail)))
 }
 
 fn uc_ident(input: &str) -> nom::IResult<&str, String> {
     let (input, head) = satisfy(is_uc_letter)(input)?;
-    let (input, tail) = take_while1(is_ident_char)(input)?;
+    let (input, tail) = take_while(is_ident_char)(input)?;
 
     Ok((input, format!("{}{}", head, tail)))
 }
@@ -145,6 +167,114 @@ fn builtin_combinator_decl(input: &str) -> nom::IResult<&str, Combinator> {
     let (input, _) = preceded(multispace0, tag(";"))(input)?;
 
     Ok((input, Combinator { id: combinator_id, r#type: combinator_type, builtin: true, constructor_number }))
+}
+
+fn var_ident(input: &str) -> nom::IResult<&str, String> {
+    alt((lc_ident, uc_ident))(input)
+}
+
+fn var_ident_opt(input: &str) -> nom::IResult<&str, String> {
+    alt((
+        var_ident,
+        map(tag("_"), |s: &str| s.to_owned())
+    ))(input)
+}
+
+fn nat_const(input: &str) -> nom::IResult<&str, &str> {
+    take_while1(is_digit)(input)
+}
+
+fn conditional_def(input: &str) -> nom::IResult<&str, String> {
+    let (input, var) = var_ident(input)?;
+    let (input, opt) = opt(preceded(tag("."), nat_const))(input)?;
+    let (input, _) = tag("?")(input)?;
+
+    match opt {
+        None => Ok((input, var)),
+        Some(opt) => Ok((input, format!("{}.{}", var, opt)))
+    }
+}
+
+fn subexpr(input: &str) -> nom::IResult<&str, String> {
+    alt((
+        term,
+        map(pair(nat_const, preceded(tag("+"), subexpr)), |(s1, s2)| format!("{}+{}", s1, s2)),
+        map(pair(subexpr, preceded(tag("+"), nat_const)), |(s1, s2)| format!("{}+{}", s1, s2))
+    ))(input)
+}
+
+fn type_ident(input: &str) -> nom::IResult<&str, String> {
+    alt((boxed_type_ident, lc_ident_ns, map(tag("#"), |s: &str| s.to_owned())))(input)
+}
+
+fn term(input: &str) -> nom::IResult<&str, String> {
+    alt((
+        delimited(tag("("), subexpr, tag(")")),
+        type_ident,
+        var_ident,
+        map(nat_const, |s| s.to_owned()),
+        preceded(tag("%"), term)),
+        // TODO
+    )(input)
+}
+
+fn type_term(input: &str) -> nom::IResult<&str, String> {
+    term(input)
+}
+
+fn args_1(input: &str) -> nom::IResult<&str, Field> {
+    let (input, id) = var_ident_opt(input)?;
+    let (input, _) = tag(":")(input)?;
+    let (input, conditional_def) = opt(conditional_def)(input)?;
+    let (input, marker) = opt(tag("!"))(input)?;
+    let (input, r#type) = type_term(input)?;
+
+
+    Ok((input, Field { name: Some(id), r#type: ConditionalField { condition: conditional_def, r#type }}))
+}
+
+fn nat_term(input: &str) -> nom::IResult<&str, String> {
+    term(input)
+}
+
+fn multiplicity(input: &str) -> nom::IResult<&str, String> {
+    terminated(nat_term, tag("*"))(input)
+}
+fn args_2(input: &str) -> nom::IResult<&str, Field> {
+    let (input, id) = opt(terminated(var_ident_opt, tag(":")))(input)?;
+    let (input, multiplicity) = opt(multiplicity)(input)?;
+    let (input, _) = tag("[")(input)?;
+    let (input, fields) = many1(args)(input)?;
+    let (input, _) = tag("]")(input)?;
+
+    Ok((input, Field { name: id, r#type: Repetition { multiplicity, fields } }))
+}
+
+fn args_3(input: &str) -> nom::IResult<&str, Vec<Field>> {
+    let (input, _) = tag("(")(input)?;
+    let (input, fields) = many1(delimited(space0, var_ident_opt, space0))(input)?;
+    let (input, _) = tag(":")(input)?;
+    let (input, mark) = opt(tag("!"))(input)?;
+    let (input, type_term) = type_term(input)?;
+    let (input, _) = tag(")")(input)?;
+
+    Ok((input, fields.into_iter().map(|id| Field {
+        name: Some(id),
+        r#type: Bare {
+            name: type_term.clone()
+        }
+    }).collect()))
+}
+
+fn args_4(input: &str) -> nom::IResult<&str, Field> {
+    let (input, mark) = opt(tag("!"))(input)?;
+    let (input, type_term) = type_term(input)?;
+
+    Ok((input, Field { name: None, r#type: Bare { name: type_term } }))
+}
+
+fn args(input: &str) -> nom::IResult<&str, Field> {
+    alt((args_1, args_2, args_4))(input)
 }
 
 #[cfg(test)]
@@ -271,6 +401,74 @@ comment ")));
     }
 
     #[test]
+    fn args_1_test() {
+        let input = "first_name:fields.0?string";
+
+        let output = args_1(input);
+
+        assert_eq!(output, Ok(("", Field { name: Some("first_name".to_owned()), r#type: ConditionalField { condition: Some("fields.0".to_owned()), r#type: "string".to_owned() }})));
+    }
+
+    #[test]
+    fn args_2_test() {
+        let input = "a:m*[n*[double]]";
+
+        let output = args_2(input);
+
+        assert_eq!(output, Ok(("", Field { name: Some("a".to_owned()), r#type: Repetition {
+            multiplicity: Some("m".to_owned()),
+            fields: vec![Field {
+                name: None,
+                r#type: Repetition {
+                    multiplicity: Some("n".to_owned()),
+                    fields: vec![Field {
+                        name: None,
+                        r#type: Bare {name: "double".to_owned()}
+                    }]
+                },
+            }]
+        }})));
+    }
+
+    #[test]
+    fn args_3_test() {
+        let input = "(x y z:int32)";
+
+        let output = args_3(input);
+
+        assert_eq!(output, Ok(("", vec![
+            Field {
+                name: Some("x".to_string()),
+                r#type: Bare {
+                    name: "int32".to_string()
+                }
+            },
+            Field {
+                name: Some("y".to_string()),
+                r#type: Bare {
+                    name: "int32".to_string()
+                }
+            },
+            Field {
+                name: Some("z".to_string()),
+                r#type: Bare {
+                    name: "int32".to_string()
+                }
+            }
+        ])));
+    }
+
+    #[test]
+    fn args_4_test() {
+        let input = "double";
+
+        let output = args_4(input);
+
+        assert_eq!(output, Ok(("", Field { name: None, r#type: Bare { name: "double".to_owned() } })));
+    }
+
+
+    #[test]
     fn empty_input() {
         let input = "";
 
@@ -342,5 +540,17 @@ boolTrue = Bool;";
             Combinator::new("boolFalse", "Bool"),
             Combinator::new("boolTrue", "Bool")]
         );
+    }
+
+    #[test]
+    fn bool_stat() {
+        let input = "// Boolean for diagonal queries
+boolStat statTrue:int statFalse:int statUnknown:int = BoolStat;";
+
+        let output = parse(input).unwrap();
+
+        assert_eq!(output, vec![
+            Combinator::new("boolStat", "BoolStat")
+        ]);
     }
 }
