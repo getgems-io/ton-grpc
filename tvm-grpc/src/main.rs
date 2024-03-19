fe0dc2bb1f5bfde6421bba611271061e40502810
Andrei Kostylev <a.kostylev@protonmail.com>
2024-03-19 13:45:10 +0300

    refac

--- tvm-grpc/src/main.rs
+++ tvm-grpc/src/main.rs
@@ -3,7 +3,9 @@ mod transaction_emulator;
 mod tvm_emulator;
 mod threaded;
 
+use std::net::SocketAddr;
 use std::time::Duration;
+use clap::Parser;
 use tonic::transport::Server;
 use tonic::codec::CompressionEncoding::Gzip;
 use tracing_subscriber::EnvFilter;
@@ -14,8 +16,26 @@ use crate::tvm::tvm_emulator_service_server::TvmEmulatorServiceServer;
 use crate::transaction_emulator::TransactionEmulatorService;
 use crate::tvm_emulator::TvmEmulatorService;
 
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[clap(long, default_value = "0.0.0.0:50052")]
+    listen: SocketAddr,
+    #[clap(long, value_parser = humantime::parse_duration, default_value = "30s")]
+    timeout: Duration,
+    #[clap(long, value_parser = humantime::parse_duration, default_value = "300s")]
+    tcp_keepalive: Duration,
+    #[clap(long, value_parser = humantime::parse_duration, default_value = "120s")]
+    http2_keepalive_interval: Duration,
+    #[clap(long, value_parser = humantime::parse_duration, default_value = "20s")]
+    http2_keepalive_timeout: Duration,
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+
     TvmEmulator::set_verbosity_level(0);
 
     tracing_subscriber::fmt()
@@ -29,9 +49,6 @@ async fn main() -> anyhow::Result<()> {
         .build()
         .unwrap();
 
-    // TODO[akostylev0] env
-    let addr = "0.0.0.0:50052".parse().unwrap();
-
     let (mut health_reporter, health_server) = tonic_health::server::health_reporter();
     health_reporter.set_serving::<TvmEmulatorServiceServer<TvmEmulatorService>>().await;
     health_reporter.set_serving::<TransactionEmulatorServiceServer<TransactionEmulatorService>>().await;
@@ -43,18 +60,19 @@ async fn main() -> anyhow::Result<()> {
         .accept_compressed(Gzip)
         .send_compressed(Gzip);
 
-    tracing::info!("Listening on {:?}", &addr);
+    tracing::info!("Listening on {:?}", &args.listen);
 
     Server::builder()
-        .tcp_keepalive(Some(Duration::from_secs(300)))
-        .http2_keepalive_interval(Some(Duration::from_secs(120)))
-        .http2_keepalive_timeout(Some(Duration::from_secs(20)))
-        .timeout(Duration::from_secs(30))
+        .timeout(args.timeout.into())
+        .tcp_keepalive(args.tcp_keepalive.into())
+        .http2_keepalive_interval(args.http2_keepalive_interval.into())
+        .http2_keepalive_timeout(args.http2_keepalive_timeout.into())
         .add_service(reflection)
         .add_service(health_server)
         .add_service(tvm_emulator_service)
         .add_service(transaction_emulator_service)
-        .serve_with_shutdown(addr, async move { tokio::signal::ctrl_c().await.unwrap(); })
+
+        .serve_with_shutdown(args.listen, async move { tokio::signal::ctrl_c().await.unwrap(); })
         .await?;
 
     Ok(())
