d005605d63a0b1a12241a1edce7936d9b34775e3
Andrei Kostylev <a.kostylev@protonmail.com>
2024-04-09 14:41:51 +0300

    feat(adnl-tcp): adnl tcp impl

---
+++ adnl-tcp/src/lib.rs
@@ -0,0 +1,5 @@
+pub mod packet;
+pub mod client;
+mod codec;
+mod key;
+pub mod ping;
---
+++ adnl-tcp/src/packet.rs
@@ -0,0 +1,47 @@
+use std::fmt::{Debug, Formatter};
+use rand::random;
+use sha2::{Digest, Sha256, digest::Update};
+
+#[derive(PartialEq, Eq)]
+pub struct Packet {
+    pub nonce: [u8; 32],
+    pub checksum: [u8; 32],
+    pub data: Vec<u8>,
+}
+
+impl Packet {
+    pub fn empty() -> Self {
+        Self::new(&[0u8; 0])
+    }
+
+    pub fn new(data: &[u8]) -> Self {
+        let nonce: [u8; 32] = random();
+        let data = data.to_vec();
+
+        let checksum: [u8; 32] = Sha256::default()
+            .chain(nonce)
+            .chain(&data)
+            .finalize()
+            .into();
+
+        Self { nonce, data, checksum }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.data.is_empty()
+    }
+
+    pub fn len(&self) -> usize {
+        self.data.len()
+    }
+}
+
+impl Debug for Packet {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Packet")
+            .field("checksum", &hex::encode(self.checksum))
+            .field("data", &hex::encode(&self.data))
+            .field("length", &self.data.len())
+            .finish()
+    }
+}
