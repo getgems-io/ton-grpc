820948edd03b61043dc43f74936a571051190ea5
Andrey Kostylev <a.kostylev@protonmail.com>
2024-05-10 18:44:33 +0300

    wip

--- adnl-tcp/src/deserializer.rs
+++ adnl-tcp/src/deserializer.rs
@@ -6,10 +6,6 @@ pub trait Deserialize where Self: Sized {
     fn deserialize(de: &mut Deserializer) -> anyhow::Result<Self>;
 }
 
-pub trait DeserializeBoxed where Self: Sized {
-    fn deserialize_boxed(de: &mut Deserializer) -> anyhow::Result<Self>;
-}
-
 pub struct Deserializer {
     input: Bytes,
     peek_constructor_number: Option<u32>
