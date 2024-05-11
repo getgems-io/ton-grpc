a62aed563bf8ea030fac475192f2083bdce7129a
Andrey Kostylev <a.kostylev@protonmail.com>
2024-05-11 21:03:27 +0300

    feat: WaitSeqno wrapper

--- ton-liteserver-client/src/request.rs
+++ ton-liteserver-client/src/request.rs
@@ -1,6 +1,7 @@
 use adnl_tcp::deserializer::DeserializeBoxed;
-use adnl_tcp::serializer::SerializeBoxed;
+use adnl_tcp::serializer::{SerializeBoxed, Serializer};
 use adnl_tcp::types::Functional;
+use crate::tl::LiteServerWaitMasterchainSeqno;
 
 pub trait Requestable: SerializeBoxed + Send {
     type Response: DeserializeBoxed + Send + 'static;
@@ -12,13 +13,28 @@ impl<T> Requestable for T
     type Response = T::Result;
 }
 
-pub struct WithSeqno<R> {
+pub struct WaitSeqno<R> {
+    prefix: LiteServerWaitMasterchainSeqno,
     request: R,
-    seqno: i32
 }
 
-impl<R> WithSeqno<R> where R: Requestable + Sized {
+impl<R> WaitSeqno<R> where R: Requestable {
     pub fn new(request: R, seqno: i32) -> Self {
-        Self { request, seqno }
+        Self::with_timeout(request, seqno, 3000)
     }
+
+    pub fn with_timeout(request: R, seqno: i32, timeout_ms: i32) -> Self {
+        Self { prefix: LiteServerWaitMasterchainSeqno { seqno, timeout_ms }, request }
+    }
+}
+
+impl<R> SerializeBoxed for WaitSeqno<R> where R: Requestable {
+    fn serialize_boxed(&self, se: &mut Serializer) {
+        self.prefix.serialize_boxed(se);
+        self.request.serialize_boxed(se);
+    }
+}
+
+impl<R> Requestable for WaitSeqno<R> where R: Requestable {
+    type Response = R::Response;
 }
