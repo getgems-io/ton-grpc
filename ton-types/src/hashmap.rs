482cf228d4c08e3cba048911d3113ac11d64a399
Andrei Kostylev <a.kostylev@protonmail.com>
2024-05-22 19:16:37 +0300

    wip

--- ton-types/src/hashmap.rs
+++ ton-types/src/hashmap.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::convert::Infallible;
 use nom::complete::{bool, take};
 use nom::IResult;
 use crate::bag_of_cells::CellInBag;
@@ -39,13 +40,16 @@ pub struct HmLabel {
 }
 
 impl HmLabel {
-    pub fn read(m: u32, input: BitInput) -> IResult<BitInput, Self> {
+    pub fn read<'a>(m: u32, input: BitInput<'a>) -> IResult<BitInput<'a>, Self> {
         let (input, bit) = bool(input)?;
         if bit {
             let (input, bit) = bool(input)?;
             if bit {
                 let (input, bit) = bool(input)?;
-                let (input, len) = take(len_bits(m))(input)?;
+                let (input, len): (_, u32) = take(len_bits(m + 1))(input)?;
+
+                println!("LENNNN: {} // {}", len, len_bits(m));
+
                 if bit {
                     Ok((input, Self { label: (1u32 << len) - 1, m, n: len }))
                 } else {
@@ -55,12 +59,11 @@ impl HmLabel {
                 let (input, len) = take(len_bits(m))(input)?;
                 let (input, label) = take(len)(input)?;
 
-                Ok((input, Self { label: label, m, n: len }))
+                Ok((input, Self { label, m, n: len }))
             }
         } else {
             let (input, Unary { n: len}) = Unary::from_bit_reader(input)?;
             let (input, label): (_, u32) = take(len)(input)?;
-            let label = label.to_le();
 
             Ok((input, Self { label: label, m, n: len }))
         }
@@ -68,7 +71,7 @@ impl HmLabel {
 }
 
 const fn len_bits(value: u32) -> u32 {
-    32 - (value - 1).leading_zeros()
+    32 - value.leading_zeros()
 }
 
 struct Hashmap<X> {
@@ -86,44 +89,45 @@ struct HashmapE<const K: u32, X> {
     inner: HashMap<u32, X>
 }
 
-// impl<const K: u32, X> HashmapE<K, X> where X: Default {
-//     fn from_bit_reader(cell: &CellInBag) -> Self {
-//         let mut inner = HashMap::new();
-//         let mut reader = LittleEndianReader::new(cell.as_ref());
-//
-//         let bit = reader.read_bit().unwrap();
-//         if bit {
-//             let root = cell.children().next().unwrap();
-//             println!("root: {:?}", root);
-//             let mut reader = LittleEndianReader::new(root.as_ref());
-//             let label = HmLabel::read(K, &mut reader);
-//             println!("label: {:?}", label);
-//
-//             let m = K - label.n;
-//             println!("m: {:?}", m);
-//             if m > 0 {
-//                 let mut iter = root.children();
-//                 let left = iter.next().unwrap();
-//                 println!("left: {:?}", left);
-//                 for c in left.children() {
-//                     println!("c: {:?}", c);
-//                 }
-//
-//                 let right = iter.next().unwrap();
-//                 println!("right: {:?}", right);
-//             } else {
-//                 let v = X::default();
-//                 // let v = X::from_bit_reader(&mut reader);
-//                 inner.insert(label.label, v);
-//             }
-//
-//             Self { inner }
-//
-//         } else {
-//             Self { inner: Default::default() }
-//         }
-//     }
-// }
+impl<const K: u32, X> HashmapE<K, X> where X: Default {
+    fn parse(cell: CellInBag) -> Self {
+        let mut inner = HashMap::new();
+        let input = (cell.as_ref(), 0);
+
+        let (_, bit) = bool::<&[u8], ()>(input).unwrap();
+        if bit {
+            let root = cell.children().next().unwrap();
+            println!("root non-empty: {:?}", root);
+
+            let input = (root.as_ref(), 0);
+            let (_, label) = HmLabel::read(K, input).unwrap();
+            println!("label: {:?}", label);
+
+            let m = K - label.n;
+            println!("m: {:?}", m);
+            if m > 0 {
+                let mut iter = root.children();
+                let left = iter.next().unwrap();
+                println!("left: {:?}", left);
+                for c in left.children() {
+                    println!("c: {:?}", c);
+                }
+
+                let right = iter.next().unwrap();
+                println!("right: {:?}", right);
+            } else {
+                let v = X::default();
+                // let v = X::from_bit_reader(&mut reader);
+                inner.insert(label.label, v);
+            }
+
+            Self { inner }
+
+        } else {
+            Self { inner: Default::default() }
+        }
+    }
+}
 
 /**
 hme_empty$0 {n:#} {X:Type} = HashmapE n X;
@@ -196,10 +200,10 @@ mod tests {
     fn hmlabel_long_test() {
         let input = vec![0b10_011_101_u8];
 
-        let (_, label) = HmLabel::read(8, (&input, 0)).unwrap();
+        let (_, label) = HmLabel::read(4, (&input, 0)).unwrap();
 
         assert_eq!(label.label, 0b00000101);
-        assert_eq!(label.m, 8);
+        assert_eq!(label.m, 4);
         assert_eq!(label.n, 3);
     }
 
@@ -207,23 +211,34 @@ mod tests {
     fn hmlabel_same_test() {
         let input = vec![0b11_1_01000_u8];
 
-        let (_, label) = HmLabel::read(32, (&input, 0)).unwrap();
+        let (_, label) = HmLabel::read(16, (&input, 0)).unwrap();
 
         assert_eq!(label.label, 0b11111111);
-        assert_eq!(label.m, 32);
+        assert_eq!(label.m, 16);
         assert_eq!(label.n, 8);
     }
 
-    // #[test]
-    // fn shard_hashes_test() {
-    //     let bytes = hex::decode("b5ee9c7201020701000110000101c0010103d040020201c0030401eb5014c376901214cdb0000152890a35b600000152890a35b85e31d8be7f5f1b44600e445b3cf778b40eaad885db5153838bea3e8f0f4a9b25e36422b74bfadf372f7d3e16b48c05f4866b05d2c7e5787bd954a5d79ad9fdb6990000450f5a00000000000000001214cd933228b81ccc8a2e52000000c90501db5014c367381214cda8000152890aafc800000152890aafcefff0db0738592205986066e14fa1221d28f0156604fd4346cea0b705712ddd2872d9dc6b6fd4eb6624bf6cb9b77d673d2df07a993f5ed281b375f3c659c25e4df80000450f5e00000000000000001214cd933228b8020600134591048ab20ee6b28020001343332bfa820ee6b28020").unwrap();
-    //     let boc = from_bytes::<BagOfCells>(&bytes).unwrap();
-    //     let root = boc.root().unwrap();
-    //
-    //     println!("root: {:?}", root);
-    //
-    //     let hashmap = HashmapE::<32, bool>::from_bit_reader(&root);
-    //
-    //     assert_eq!(hashmap.inner.len(), 2);
-    // }
+    #[test]
+    fn hmlabel_same_real_test() {
+        let input = hex::decode("d000").unwrap();
+
+        let (_, label) = HmLabel::read(32, (&input, 0)).unwrap();
+
+        assert_eq!(label.label, 0);
+        assert_eq!(label.m, 32);
+        assert_eq!(label.n, 32);
+    }
+
+    #[test]
+    fn shard_hashes_test() {
+        let bytes = hex::decode("b5ee9c7201020701000110000101c0010103d040020201c0030401eb5014c376901214cdb0000152890a35b600000152890a35b85e31d8be7f5f1b44600e445b3cf778b40eaad885db5153838bea3e8f0f4a9b25e36422b74bfadf372f7d3e16b48c05f4866b05d2c7e5787bd954a5d79ad9fdb6990000450f5a00000000000000001214cd933228b81ccc8a2e52000000c90501db5014c367381214cda8000152890aafc800000152890aafcefff0db0738592205986066e14fa1221d28f0156604fd4346cea0b705712ddd2872d9dc6b6fd4eb6624bf6cb9b77d673d2df07a993f5ed281b375f3c659c25e4df80000450f5e00000000000000001214cd933228b8020600134591048ab20ee6b28020001343332bfa820ee6b28020").unwrap();
+        let boc = from_bytes::<BagOfCells>(&bytes).unwrap();
+        let root = boc.root().unwrap();
+
+        println!("root: {:?}", root);
+
+        let hashmap = HashmapE::<32, bool>::parse(root);
+
+        assert_eq!(hashmap.inner.len(), 2);
+    }
 }
