09d31c1cc749a76a621185de8b6755548de3e722
i.nedzvetskiy <i.nedzvetskiy@gmail.com>
2026-01-22 15:03:12 +0300

    wip(ton-grpc): try fix build 21

--- tonlibjson-sys/build.rs
+++ tonlibjson-sys/build.rs
@@ -138,6 +138,12 @@ fn main() {
     )
     .unwrap();
 
+    let use_old_private_lib = if cfg!(feature = "testnet") {
+      false
+    } else {
+      true
+    };
+
     if cfg!(feature = "tonlibjson") {
         let dst = cfg.build_target("tonlibjson").build();
 
@@ -214,7 +220,7 @@ fn main() {
             dst.display()
         );
         println!("cargo:rustc-link-lib=static=tonlib");
-        if cfg!(feature != "testnet") {
+        if use_old_private_lib {
           // tonlibjson_private was removed from this commit
           // https://github.com/ton-blockchain/ton/commit/ddb173b16f4ff8fb314175b9751720dbfc79e77e
           println!("cargo:rustc-link-lib=static=tonlibjson_private");
