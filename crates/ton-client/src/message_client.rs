cec783e12c090a8132f38ae27e54d8c962c53107
Andrei Kostylev <a.kostylev@protonmail.com>
2026-06-10 21:04:46 +0300

    cleanup

--- crates/ton-client/src/message_client.rs
+++
@@ -1,8 +0,0 @@
-use async_trait::async_trait;
-
-#[async_trait]
-pub trait MessageClient: Clone + Send + Sync + 'static {
-    async fn send_message(&self, message: &str) -> anyhow::Result<()>;
-
-    async fn send_message_returning_hash(&self, message: &str) -> anyhow::Result<String>;
-}
