67ca61d324edd820462f239a2149a7e634826741
Andrei Kostylev <a.kostylev@protonmail.com>
2026-07-14 18:16:03 +0300

    feat: client impl flag (#1637)
    * feat: ton-grpc `--client` flag

--- crates/ton-grpc/src/main.rs
+++ crates/ton-grpc/src/main.rs
@@ -1,23 +1,31 @@
-use clap::{Args, Parser};
+use clap::{Args, Parser, ValueEnum};
 use humantime::parse_duration;
 use metrics_exporter_prometheus::PrometheusBuilder;
 use std::net::SocketAddr;
 use std::path::PathBuf;
 use std::time::Duration;
-use ton_client::{ConfigSource, PoolTransport, TonClientBuilder};
-use ton_config::default_ton_config_url;
+use ton_client::{ConfigSource, PoolTransport, TonClientBuilder, TonService};
+use ton_config::{TonConfig, default_ton_config_url};
 use ton_grpc::AccountService;
 use ton_grpc::BlockService;
 use ton_grpc::MessageService;
 use ton_grpc::account_service_server::AccountServiceServer;
 use ton_grpc::block_service_server::BlockServiceServer;
 use ton_grpc::message_service_server::MessageServiceServer;
+use ton_liteserver_client::MakeLiteServerAdapter;
 use tonic::codec::CompressionEncoding::Gzip;
 use tonic::transport::Server;
 use tonlibjson_client::MakeTonlibjsonAdapter;
+use tower::Service;
 use tracing_subscriber::EnvFilter;
 use tracing_subscriber::fmt::format::FmtSpan;
 use url::Url;
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, ValueEnum)]
+enum ClientImpl {
+    Tonlibjson,
+    AdnlTcp,
+}
 
 #[derive(Args, Debug)]
 #[group(multiple = false)]
@@ -64,6 +72,9 @@ struct AppArgs {
     #[clap(long, default_value = "0.0.0.0:9000")]
     metrics_listen: SocketAddr,
 
+    #[clap(long, value_enum, default_value_t = ClientImpl::Tonlibjson)]
+    client: ClientImpl,
+
     #[clap(flatten)]
     ton_config_args: TonConfigArgs,
     #[clap(long, value_parser = parse_duration, default_value = "10s")]
@@ -103,17 +114,38 @@ async fn main() -> anyhow::Result<()> {
         tracing::info!("Listening metrics on {:?}", &args.metrics_listen);
     }
 
-    let mut client =
-        TonClientBuilder::<MakeTonlibjsonAdapter>::from_config_source(args.ton_config_args)
-            .set_timeout(args.ton_timeout)
-            .set_retry_budget_ttl(args.retry_budget_ttl)
-            .set_retry_min_per_sec(args.retry_min_rps)
-            .set_retry_percent(args.retry_withdraw_percent)
-            .set_retry_first_delay(args.retry_first_delay)
-            .set_retry_max_delay(args.retry_max_delay)
-            .set_ewma_default_rtt(args.ewma_default_rtt)
-            .set_ewma_decay(args.ewma_decay)
-            .build()?;
+    match args.client {
+        ClientImpl::Tonlibjson => serve(args, MakeTonlibjsonAdapter).await,
+        ClientImpl::AdnlTcp => serve(args, MakeLiteServerAdapter).await,
+    }
+}
+
+async fn serve<F>(args: AppArgs, factory: F) -> anyhow::Result<()>
+where
+    F: Service<TonConfig, Response: TonService, Error: Send + Sync, Future: Send + Unpin>
+        + Clone
+        + Send
+        + 'static,
+    anyhow::Error: From<F::Error>,
+{
+    let config_source = ConfigSource::from(args.ton_config_args);
+    match &config_source {
+        ConfigSource::File { path } => tracing::info!("TON Config path: {}", path.display()),
+        ConfigSource::Url { url, .. } => tracing::info!("TON Config URL: {}", url),
+        ConfigSource::Config { .. } => tracing::info!("TON Config: inline"),
+    }
+    tracing::info!("Client implementation: {:?}", &args.client);
+
+    let mut client = TonClientBuilder::<F>::with_factory_and_source(factory, config_source)
+        .set_timeout(args.ton_timeout)
+        .set_retry_budget_ttl(args.retry_budget_ttl)
+        .set_retry_min_per_sec(args.retry_min_rps)
+        .set_retry_percent(args.retry_withdraw_percent)
+        .set_retry_first_delay(args.retry_first_delay)
+        .set_retry_max_delay(args.retry_max_delay)
+        .set_ewma_default_rtt(args.ewma_default_rtt)
+        .set_ewma_decay(args.ewma_decay)
+        .build()?;
 
     client.wait_ready().await?;
     tracing::info!("Ton Client is ready");
@@ -133,17 +165,15 @@ async fn main() -> anyhow::Result<()> {
         .accept_compressed(Gzip)
         .send_compressed(Gzip);
 
-    type Client = PoolTransport<MakeTonlibjsonAdapter>;
-
     let (health_reporter, health_server) = tonic_health::server::health_reporter();
     health_reporter
-        .set_serving::<AccountServiceServer<AccountService<Client>>>()
+        .set_serving::<AccountServiceServer<AccountService<PoolTransport<F>>>>()
         .await;
     health_reporter
-        .set_serving::<BlockServiceServer<BlockService<Client>>>()
+        .set_serving::<BlockServiceServer<BlockService<PoolTransport<F>>>>()
         .await;
     health_reporter
-        .set_serving::<MessageServiceServer<MessageService<Client>>>()
+        .set_serving::<MessageServiceServer<MessageService<PoolTransport<F>>>>()
         .await;
 
     tracing::info!("Listening on {:?}", &args.listen);
